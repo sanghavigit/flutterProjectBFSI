79193f64315ab53388dca22b01f08c84f116f2f6
Sanghavi Vavekar <sanghavivavekar@gmail.com>
2026-01-31 00:12:05 +0530

    test cases

--- test/auth/state/auth_cubit_test.dart
+++ test/auth/state/auth_cubit_test.dart
@@ -2,7 +2,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:flutter_project_bfsi/auth/data/auth_repository.dart';
-import 'package:flutter_project_bfsi/auth/security/secure_storage_service.dart';
 import 'package:flutter_project_bfsi/auth/state/auth_cubit.dart';
 import 'package:flutter_project_bfsi/auth/state/auth_state.dart';
 import '../../mocks/mock_repositories.dart';
